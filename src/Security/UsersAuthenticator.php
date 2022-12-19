ae6392b7503049e055f7f4ed59316db84a551412
Ethan LERIGAB <ethanlerigab@gmail.com>
2022-12-19 16:17:16 +0100

    tentative de commit sur dev

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -47,8 +47,8 @@ class UsersAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('app_test'.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_test'));
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
